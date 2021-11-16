76a81578ae3b51b3be3f1a875578a15e882aff0a
SbasOff <92762587+SbasOff@users.noreply.github.com>
2021-11-16 17:02:18 -0600

    Version 2

---
+++ Algorimo_simplex/src/algorimo_simplex/Algorimo_simplex.java
@@ -0,0 +1,11 @@
+package algorimo_simplex;
+
+public class Algorimo_simplex {
+
+    
+    public static void main(String[] args) {
+        simplex_vista v = new simplex_vista ();
+            v.setVisible(true );
+    }
+    
+}
