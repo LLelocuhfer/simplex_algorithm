29ace1f37ffd3a98f94e88c95c6f43c6c6ed29c8
Jesus Mendez <92119524+Jedez@users.noreply.github.com>
2021-11-21 21:54:01 -0600

    Delete simplex directory

--- simplex/src/main/java/metodo_simplex/ventanaPrincipla.java
+++
@@ -1,9 +0,0 @@
-package metodo_simplex;
-
-
-public class ventanaPrincipla {
-    public static void main (String args[]){
-            princiPal ventanaPrincipal = new princiPal();
-        ventanaPrincipal.setVisible(true);
-    }
-}
